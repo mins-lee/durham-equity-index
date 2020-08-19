4640292087d245a603c5185f24b1c74dfae27222
Minseon Lee <minnms.lee@gmail.com>
2020-08-19 16:18:33 -0400

    added equity scores

--- durhamequity.py
+++ durhamequity.py
@@ -2,16 +2,10 @@ import pandas as pd
 import censusdata
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.precision', 2)
-# variables: 
-# People with Disabilities, People in Poverty, Minority Race and Ethnicity 
-# Persons, Households without Vehicles, Older Adults, Persons under Age 18, 
-# Persons with Limited English Proficiency, Female Householders 
-
-def showVar(table):
-    return censusdata.printtable(censusdata.censustable('acs5', 2018, table))
 
 # assign acs table numbers 
-poverty = 'B19001'
+income = 'B19001'
+rent = 'B03002'
 minority = 'B03002'
 vehicle = 'B08141'
 older = 'B11007'
@@ -20,7 +14,91 @@ disability = 'B18101'
 language = 'B16001'
 femaleHouseholder = 'B17010'
 
-# here: alternative
+categories = [income, rent, minority, vehicle, older, younger, disability, 
+                language, femaleHouseholder]
+
+def genTable(table):
+    return censusdata.printtable(censusdata.censustable('acs1', 2018, table))
+
+def printTables():
+    for name in categories: 
+        genTable(name)
+
+# printTables()
+# print(genTable('B16002')) # here: why the package does not compatible with C  
+
+income_var = ['B19001_002E', 'B19001_003E', 'B19001_004E', 'B19001_005E', 
+            'B19001_001E']
+rent_var = ['B25070_007E', 'B25070_008E', 'B25070_009E', 'B25070_010E', 
+            'B25070_001E', 'B25070_011E']
+minority_var = ['B03002_001E', 'B03002_003E', 'B03002_001E'] 
+older_var = ['B11007_002E', 'B11007_001E']      
+younger_var = ['B11005_002E', 'B11005_001E']  
+femaleHouseholder_var = ['B17010_016E', 'B17010_036E', 'B17010_001E']
+
+all_var = income_var + rent_var + minority_var + older_var + younger_var + femaleHouseholder_var
+# durhambg = ""
+
+year = 2018
+nc = '37'
+durham = '063'
+def genBlockGroupData(var):
+    return censusdata.download('acs5', year, censusdata.censusgeo([
+        ('state', nc), ('county', durham), ('block group', '*')]), var)
+
+# def printBlockGroupData(var):
+#     durham = genBlockGroupData(var)
+#     return durham.B25070_007E.head(5)
+
+# print(printBlockGroupData(var1))
+
+index = genBlockGroupData(all_var)
+
+# compute variables
+index['income'] = (index.B19001_002E + index.B19001_003E + 
+        index.B19001_004E + index.B19001_005E) / (index.B19001_001E) * 100
+index['rent'] = (index.B25070_007E + index.B25070_008E + index.B25070_009E + 
+        index.B25070_010E) / (index.B25070_001E - index.B25070_011E) * 100
+index['minority'] = (index.B03002_001E - index.B03002_003E) / (index.B03002_001E) * 100
+index['older'] = (index.B11007_002E) / (index.B11007_001E) * 100
+index['younger'] = (index.B11005_002E) / (index.B11005_001E) * 100
+index['femaleHouseholder'] = (index.B17010_016E + index.B17010_036E) / (index.B17010_001E) * 100
+
+index = index[['income', 'rent', 'minority', 'older', 'younger', 'femaleHouseholder']]
+
+maxVal = []
+percent = ['income', 'rent', 'minority', 'older', 'younger', 'femaleHouseholder']
+for i in range(len(percent)): 
+    maxVal += [index.describe()[percent[i]]['max']]
+
+score = index.copy().div(maxVal)
+score['mean'] = score.mean(axis=1)
+
+# show 10 block groups with highest equity score
+print(score.sort_values('mean', ascending=False).head(10))
+
+
+
+# here: figure out the two other factors
+# find block group names
+# change table name from index to something else
+
+
+# print(score.head(3))
+# print(score.columns)
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 1-1. poverty: HOUSEHOLD INCOME IN THE PAST 12 months
 # (Less than $10,000 + $10,000 to $14,999 + $15,000 to $19,999 + 
 # $20,000 to $24,999)/(Total) * 100
@@ -59,9 +137,9 @@ var5 = ['B11007_002E', 'B11007_001E']
 
 # 5. younger: HOUSEHOLDS BY PRESENCE OF PEOPLE UNDER 18 YEARS BY HOUSEHOLD TYPE
 # (Households with one or more people under 18 years)/(Total) * 100
-# ('B11005_002E')/('B11005_002E') * 100
+# ('B11005_002E')/('B11005_001E') * 100
 
-var6 = ['B11005_002E', 'B11005_002E']
+var6 = ['B11005_002E', 'B11005_001E']
 
 # 6. disability: SEX BY AGE BY DISABILITY STATUS
 # (Male with disability by age + Female with disability by age)/Total * 100
@@ -106,17 +184,9 @@ var = generateVarNames()
 
 
 
-# test test_var
 
-allVar = [poverty, minority, vehicle, older, younger, disability, language, femaleHouseholder]
 
-def printTables():
-    for name in allVar: 
-        showVar(name)
 
-# printTables()
-
-# print(showVar('C16002')) # here: why the package does not compatible with C  
 
 
 # household income in the past
@@ -140,25 +210,11 @@ def printTables():
 # test_var = ['B08141_001E', 'B08141_002E', 'B08141_003E', 'B08141_004E', 'B08141_005E']
 
 
-# here
-# download durham data 
-durhambg = censusdata.download('acs5', 2018, 
-            censusdata.censusgeo([('state', '37'), ('county', '063'), ('block group', '*')]),
-            var9)
-
-# here: check for all other variables
-print(durhambg.head(5))
 
 
-# compute variables
-durhambg['total'] = durhambg.B08141_001E 
-durhambg['categories'] = (durhambg.B08141_002E + durhambg.B08141_003E + 
-                        durhambg.B08141_004E + durhambg.B08141_005E)
-durhambg = durhambg[['total', 'categories']]
-durhambg.describe()
 
-# show 30 block groups with highest rate of unemployment 
-# print(durhambg.sort_values('total', ascending=False).head(30))
+
+
 
 acsTable = pd.read_excel('ACS2018_Table_Shells.xlsx')
 
