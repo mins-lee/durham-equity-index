84a3a9da314d8f7f89dd08cdce89386580307ed8
Minseon Lee <minnms.lee@gmail.com>
2020-12-11 11:29:51 -0500

    upload plot

---
+++ plot.py
@@ -0,0 +1,27 @@
+#%%
+import equityscore
+import geopandas as gpd
+import json
+import cufflinks as cf
+import pandas as pd
+cf.go_offline() 
+
+df = equityscore.main()
+#%%
+# plot bar graph
+df['equity'].value_counts().iplot(kind='bar',
+    title='Equity score distribution for Durham County, NC',
+    xTitle='Equity Score', yTitle='Num of block groups')
+
+#%%
+low = df[df['equity'] == 'low'].censusTract.value_counts()
+medium = df[df['equity'] == 'medium'].censusTract.value_counts()
+high = df[df['equity'] == 'high'].censusTract.value_counts()
+temp = pd.DataFrame([low, medium, high], index=['low', 'medium', 'high'])
+temp.iplot(kind='bar', barmode='stack', title='Equity Score by Census Area')
+
+# %%
+
+
+
+
