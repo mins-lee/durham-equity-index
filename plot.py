4da8b81ab8d69b01af8368e11bc621397cf48f1d
Minseon Lee <minnms.lee@gmail.com>
2020-12-11 14:35:21 -0500

    add comment

--- plot.py
+++ plot.py
@@ -8,20 +8,15 @@ cf.go_offline()
 
 df = equityscore.main()
 #%%
-# plot bar graph
+# plot histogram
 df['equity'].value_counts().iplot(kind='bar',
     title='Equity score distribution for Durham County, NC',
     xTitle='Equity Score', yTitle='Num of block groups')
 
 #%%
+# plot equity score by block group
 low = df[df['equity'] == 'low'].censusTract.value_counts()
 medium = df[df['equity'] == 'medium'].censusTract.value_counts()
 high = df[df['equity'] == 'high'].censusTract.value_counts()
 temp = pd.DataFrame([low, medium, high], index=['low', 'medium', 'high'])
 temp.iplot(kind='bar', barmode='stack', title='Equity Score by Census Area')
-
-# %%
-
-
-
-
